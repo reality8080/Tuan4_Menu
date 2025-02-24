054601f3c00fef5b5c56bcc101fd1690bce6ab18
reality8080 <155755865+reality8080@users.noreply.github.com>
2025-02-24 19:49:45 +0700

    Add project files.

---
+++ SQLQueryMenu.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE MENU
+GO
+
+USE MENU
+GO
+
+CREATE TABLE menu(
+	id int IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) not null,
+	price float not null,
+	watch DATE not null
+);
+GO
+
+CREATE PROCEDURE selectAll
+AS
+BEGIN
+	SELECT * FROM menu
+END;
+GO
+
+CREATE PROCEDURE insertFood
+	@name NVARCHAR(100),
+	@price float,
+	@watch DATE
+AS
+BEGIN
+	INSERT INTO menu(name,price,watch)
+	VALUES (@name,@price,@watch);
+END;
+GO
+
+CREATE PROCEDURE updateFood
+	@id int,
+	@name NVARCHAR(100),
+	@price float,
+	@watch DATE
+AS
+BEGIN
+	UPDATE menu
+	SET name=@name,price=@price, watch=@watch
+	WHERE id=@id;
+END;
+GO
+
+EXEC sp_helptext 'insertFood'
